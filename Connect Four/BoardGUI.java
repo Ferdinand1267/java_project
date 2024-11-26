a5b573e0049f961940b399ee877fbe502524d1bf
Ferdinand1267 <ferdinand.changb2026@stu.tes.tp.edu.tw>
2024-11-26 16:10:49 +0000

    new files

--- Connect Four/BoardGUI.java
+++ Connect Four/BoardGUI.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Board extends JPanel{
+public class BoardGUI extends JFrame{
     
 }
---
+++ Connect Four/Logic.java
@@ -0,0 +1,7 @@
+public class Board{
+    private int row = 6;
+    private int col = 7;
+    private char[][] board = new char[row][col];
+
+    
+}
