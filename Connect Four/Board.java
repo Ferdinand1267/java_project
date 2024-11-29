f72c923acd0e9a8349a1dec8ec37e4e329d977b5
Ferdinand1267 <ferdinand.changb2026@stu.tes.tp.edu.tw>
2024-11-29 10:03:44 +0800

    add code to school laptop

---
+++ Connect Four/Board.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Board extends JFrame{
+    private int row = 6;
+    private int col = 7;
+    private int cellSize = 100;
+    
+    
+}
