27a73420e9412ec36b9d3b1daf79dc9e645579cb
Ferdinand1267 <ferdinand.changb2026@stu.tes.tp.edu.tw>
2024-11-30 09:11:30 +0000

    checkwin method

--- Connect Four/Logic.java
+++ Connect Four/Logic.java
@@ -17,4 +17,34 @@ public class Logic{
         yellowWins = false;
     }
     
+    public boolean checkWin(){
+        gameOver = false;
+        redWins = false;
+        yellowWins = false;
+        char[][] gameBoard = board.getBoard();
+        char turn = board.getTurn();
+        if (checkRows(gameBoard,turn) || checkColumns(gameBoard,turn) || checkDiagonal(gameBoard,turn)){
+            if(turn == 'r'){
+                redWins = true;
+                gameOver = true;
+                return true;
+            }
+            else{
+                yellowWins = true;
+                gameOver = true;
+                return true;
+            }
+        }
+        else{
+            for (int i = 0;i<gameBoard.length;i++){
+                if(gameBoard[0][gameBoard[0].length-1]==' '){
+                    break;
+                }
+                else if(i == gameBoard.length - 1){
+                    gameOver = true;
+                    return true;
+                }
+            }
+        }
+    }
 }
